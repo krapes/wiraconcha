b17c7cfa75a215337413692784a5be92871bf3d6
krapes <kerri.rapes@gmail.com>
2020-05-20 10:30:40 -0400

    Refactor: Move cloudFunction helper to gcp_helper

--- src/gcp_helper.py
+++ src/gcp_helper.py
@@ -2,6 +2,8 @@ from google.cloud import storage
 import os
 import logging
 import sys
+import json
+
 
 
 def download_data(bucket_name, gcs_path, local_path, verbose=False):
@@ -98,3 +100,26 @@ def setupLogger():
 	root.info("function_identity: {}".format(function_identity))
 	if function_identity == 'local':
 		logging.StreamHandler(sys.stdout)
+
+
+def get_project_id():
+	""" Gets the project ID. It defaults to the project declared in the
+		enviorment variable PROJECT but if it can't find it there it will
+		try looking for a service account and take the project ID from there
+
+		Args:
+		Returns:
+	"""
+	service_acc_address = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', None)
+	if service_acc_address:
+		service_acc = open(service_acc_address, 'r').read()
+		service_acc_project_id = json.loads(service_acc)['project_id']
+	else:
+		service_acc_project_id = None
+	project_id = os.environ.get('PROJECT', service_acc_project_id)
+
+	if service_acc_project_id != None and project_id != service_acc_project_id:
+		logging.critical("Warning the project in ENV VAR PROJECT is \
+			not the same as your service account project")
+
+	return project_id
